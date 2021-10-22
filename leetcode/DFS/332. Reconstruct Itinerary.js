1ee1b32d5f513ad08e2b8b999ff66cf271dae917
DESKTOP-UI5I6K2\jang <myeongji_jang@tmax.co.kr>
2021-10-22 16:11:43 +0900

    dfs: 332. Reconstruct Itinerary

---
+++ leetcode/DFS/332. Reconstruct Itinerary.js
@@ -0,0 +1,56 @@
+/**
+ * /*
+ * You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. Reconstruct the itinerary in order and return it.
+ *
+ * All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, you should return the itinerary that has the smallest lexical order when read as a single string.
+ *
+ * For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+ * You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+ * Output: ["JFK","MUC","LHR","SFO","SJC"]
+ * Example 2:
+ *
+ *
+ * Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+ * Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+ * Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
+ *
+ * @format
+ */
+
+// 되게 많이 풀어본 문제
+// 모든 정점을 한번씩 도달하는 해밀턴 경로
+// 그래프에서 재귀 구조 코드
+// 많이 풀지만 매번 lexical 부분에서 실패..
+var findItinerary = function (tickets) {
+    let map = {};
+    let res = [];
+    for (let i = 0; i < tickets.length; i++) {
+        let dep = tickets[i][0];
+        let des = tickets[i][1];
+        if (map[dep]) {
+            map[dep].push(des);
+        } else {
+            //map에 값 없으면
+            map[dep] = [des];
+        }
+    }
+    for (let loc in map) {
+        map[loc].sort();
+    }
+    const dfs = (node) => {
+        let des = map[node];
+        while (des && des.length > 0) {
+            dfs(des.shift());
+        }
+        res.push(node);
+    };
+    dfs('JFK');
+    return res.reverse();
+};
