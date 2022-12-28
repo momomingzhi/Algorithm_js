51efb3c7144c651e99d086d074737df25b207b18
mong <momomingzhi@gmail.com>
2022-12-28 10:13:22 +0000

    Update 9935.js and input.txt

--- 백준/stack2/9935.js
+++ 백준/stack2/9935.js
@@ -1,10 +1,49 @@
+/*
+12ab112ab2ab
+12ab
+
+ */
 var fs = require("fs");
 let [arr, bomb] = fs.readFileSync("./input.txt").toString().trim().split("\n");
-let stack = arr.split("");
-
-while (arr.includes(bomb)) {
-  const idx = arr.indexOf(bomb);
-  stack.splice(idx, bomb.length);
-  arr = stack.join("");
+let stack = [];
+let bombIdx = 0;
+let flag = false;
+for(let i=0;i<arr.length;i++){
+    
+    if(bomb[bombIdx] === arr[i]){
+        flag = true;
+        console.log('같음',arr[i],bomb[bombIdx],i,stack,bombIdx)
+        if(bombIdx === bomb.length -1){
+            //처음부터 끝까지 값이 같다
+            stack.pop();
+            //bombIdx = stack[stack.length - 1].length -1;
+            //stack.push('')
+            if(!stack.length){
+                bombIdx = 0;
+            }else
+                bombIdx = stack[stack.length - 1].length;
+        }else{
+            if(!stack.length){
+                stack.push(arr[i]);
+            }else{
+                stack[stack.length - 1] += arr[i]
+            }    
+            bombIdx++;
+        }
+        
+    }else{
+        flag = false;
+        console.log('#####:',stack,arr[i],flag,bombIdx)
+        if(!stack.length || (!flag && bombIdx)){
+            stack.push(arr[i]);
+        }else{
+            stack[stack.length - 1] += arr[i]
+            bombIdx = 0;
+        }
+        
+    }
+    console.log({stack})
 }
-console.log(arr.length ? arr : "FRULA");
+
+
+console.log(stack)
