420eaba7a7508091f21564f7a44768de1087ba6c
mong <momomingzhi@gmail.com>
2022-12-29 11:02:14 +0000

    Update 9935.js and input.txt

--- 백준/stack2/9935.js
+++ 백준/stack2/9935.js
@@ -7,43 +7,61 @@ var fs = require("fs");
 let [arr, bomb] = fs.readFileSync("./input.txt").toString().trim().split("\n");
 let stack = [];
 let bombIdx = 0;
-let flag = false;
-for(let i=0;i<arr.length;i++){
-    
-    if(bomb[bombIdx] === arr[i]){
-        flag = true;
-        console.log('같음',arr[i],bomb[bombIdx],i,stack,bombIdx)
-        if(bombIdx === bomb.length -1){
-            //처음부터 끝까지 값이 같다
-            stack.pop();
-            //bombIdx = stack[stack.length - 1].length -1;
-            //stack.push('')
-            if(!stack.length){
+let idx = 0;
+let totalLen = arr.length - 1;
+let ddd = 0;
+let arrStack = [];
+while(1){
+    if(ddd === totalLen) break;
+    if(arr[idx] == bomb[bombIdx]){
+       // console.log('1:',bombIdx,arr[idx],stack,arrStack)
+        if(bomb.length -1 == bombIdx){
+         //   idx += bombIdx;
+            
+            stack.pop()
+            if(stack[stack.length - 1] && stack[stack.length - 1].length){
+             //   console.log('이거: ',stack[stack.length - 1].length)
+                bombIdx = stack[stack.length - 1]?.length;
+            }
+            else
                 bombIdx = 0;
-            }else
-                bombIdx = stack[stack.length - 1].length;
-        }else{
-            if(!stack.length){
-                stack.push(arr[i]);
-            }else{
-                stack[stack.length - 1] += arr[i]
-            }    
+          //  console.log('ddd',arr)
+        }
+        else if(bombIdx == 0){
+            stack.push(arr[idx]);
             bombIdx++;
         }
-        
-    }else{
-        flag = false;
-        console.log('#####:',stack,arr[i],flag,bombIdx)
-        if(!stack.length || (!flag && bombIdx)){
-            stack.push(arr[i]);
-        }else{
-            stack[stack.length - 1] += arr[i]
-            bombIdx = 0;
+            
+        else{
+            stack[stack.length - 1] += arr[idx];
+            bombIdx++;
         }
+            
         
+        idx++;
+      //  console.log('나옴:',bombIdx,idx,stack,arrStack)
+        continue;
     }
-    console.log({stack})
+    if(stack[stack.length - 1] && stack[stack.length - 1]?.length -1 !== bombIdx){
+        //c4중에서 다음에 4가 들어올것을 예상했는데 c가 들어온다면?
+        // 다음에 
+      //  console.log('2:',bombIdx,arr[idx],stack,arrStack)
+        stack.push(arr[idx])
+        bombIdx = 1;
+    }
+    else{
+      //  console.log('3:',stack[stack.length - 1],bombIdx,bomb[bombIdx],arr[idx],bomb[bombIdx]==arr[idx],stack,arrStack)
+        arrStack.push(arr[idx]);  
+    }
+      
+        
+    
+    idx++;  
+    ddd++;
 }
 
-
-console.log(stack)
+const res = arrStack.join('');
+if(!res.length)
+    console.log('FRULA')
+else
+    console.log(res.join(''))
