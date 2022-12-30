5b2b1e7d204fd89d1783117770346f8df477b0a0
mong <momomingzhi@gmail.com>
2022-12-30 00:03:53 +0000

    Update 9935.js

--- 백준/stack2/9935.js
+++ 백준/stack2/9935.js
@@ -4,64 +4,35 @@
 
  */
 var fs = require("fs");
-let [arr, bomb] = fs.readFileSync("./input.txt").toString().trim().split("\n");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let [str, bomb] = fs.readFileSync('./input.txt').toString().trim().split("\n");
 let stack = [];
-let bombIdx = 0;
-let idx = 0;
-let totalLen = arr.length - 1;
-let ddd = 0;
-let arrStack = [];
-while(1){
-    if(ddd === totalLen) break;
-    if(arr[idx] == bomb[bombIdx]){
-       // console.log('1:',bombIdx,arr[idx],stack,arrStack)
-        if(bomb.length -1 == bombIdx){
-         //   idx += bombIdx;
-            
-            stack.pop()
-            if(stack[stack.length - 1] && stack[stack.length - 1].length){
-             //   console.log('이거: ',stack[stack.length - 1].length)
-                bombIdx = stack[stack.length - 1]?.length;
+let len = bomb.length - 1;
+for(let i=0;i<str.length;i++){
+    let flag = false;
+    if(str[i] === bomb[len]){
+        //c4의 마지막글자와 같을 경우
+        for(let j=0;j<len ;j++){
+            if(stack[stack.length - (j+1)] === bomb[len - (j+1)]){
+                continue;
             }
-            else
-                bombIdx = 0;
-          //  console.log('ddd',arr)
+            flag = true;
+            break;
         }
-        else if(bombIdx == 0){
-            stack.push(arr[idx]);
-            bombIdx++;
+        if(flag){
+            stack.push(str[i]);
+        }else{
+            for(let k=0;k<len;k++){
+                stack.pop();
+            }
         }
-            
-        else{
-            stack[stack.length - 1] += arr[idx];
-            bombIdx++;
-        }
-            
-        
-        idx++;
-      //  console.log('나옴:',bombIdx,idx,stack,arrStack)
-        continue;
+    }else{
+        stack.push(str[i]);
     }
-    if(stack[stack.length - 1] && stack[stack.length - 1]?.length -1 !== bombIdx){
-        //c4중에서 다음에 4가 들어올것을 예상했는데 c가 들어온다면?
-        // 다음에 
-      //  console.log('2:',bombIdx,arr[idx],stack,arrStack)
-        stack.push(arr[idx])
-        bombIdx = 1;
-    }
-    else{
-      //  console.log('3:',stack[stack.length - 1],bombIdx,bomb[bombIdx],arr[idx],bomb[bombIdx]==arr[idx],stack,arrStack)
-        arrStack.push(arr[idx]);  
-    }
-      
-        
-    
-    idx++;  
-    ddd++;
 }
-
-const res = arrStack.join('');
-if(!res.length)
-    console.log('FRULA')
-else
-    console.log(res.join(''))
+let res = stack.join('');
+if(res === ''){
+    console.log('FRULA');
+}else{
+    console.log(stack.join(''))
+}
